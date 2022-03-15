15a139b5d8dafb515898a6251e43b525d546cf80
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-03-15 23:29:24 +0900

    Redux Counter

--- src/index.js
+++ src/index.js
@@ -1,26 +1,37 @@
+import {createStore} from 'redux';
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
 
-let count = 0;
+const ADD = "ADD";
+const MINUS = "MINUS";
+const countModifier = (count = 0,action) =>{
+ switch(action.type){
+   case ADD:
+     return count +1;
+    case MINUS :
+      return count -1;
+    default:
+      return count;
+ }
+};
 
-number.innerText = count;
+const countStore = createStore(countModifier);
+const onChange = () =>{
+  number.innerText = countStore.getState();
+};
 
-const updateText = () =>{
-  number.innerText = count;
+countStore.subscribe(onChange);
+
+
+const handleAdd = () =>{
+  countStore.dispatch({type: ADD });
 }
-
-const handleAdd =() =>{
-  console.log("add");
-  count = count + 1;
-  updateText();
-
-}
-const handleMinus =()=>{
- console.log("minus");
-  count = count -1;
-  updateText();
+const handleMinus = () =>{
+  countStore.dispatch({type: MINUS });
 }
 add.addEventListener("click",handleAdd);
 minus.addEventListener("click",handleMinus);
